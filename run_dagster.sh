7620a7f13a7a22fc220f509795dd99b0924de92b
Hernán Ordiales <hordiales@gmail.com>
2025-03-02 23:06:01 -0300

    * Code update. Orchestration working * MLFlow metrics asset working * Dependencies update

---
+++ run_dagster.sh
@@ -0,0 +1,2 @@
+export DAGSTER_HOME=$PWD/dagster_home
+dagster dev
---
+++ run_mlflow.sh
@@ -0,0 +1,12 @@
+
+export REPO_FOLDER=$PWD/../mlops-ecosystem
+export postgres_data_folder=$REPO_FOLDER/dbs/data/postgres
+export mongo_data_folder=$REPO_FOLDER/dbs/data/mongo
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=mysecretpassword
+export POSTGRES_HOST=localhost:5432
+export MLFLOW_POSTGRES_DB=mlflow_db
+
+export MLFLOW_ARTIFACTS_PATH=$PWD/mlflow_data
+
+mlflow server --backend-store-uri postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$MLFLOW_POSTGRES_DB --default-artifact-root $MLFLOW_ARTIFACTS_PATH -h 0.0.0.0 -p 8002

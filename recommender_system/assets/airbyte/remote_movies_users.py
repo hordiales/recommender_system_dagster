c692416a61eb09831ce051641aa53d766086436c
Hernán Ordiales <hordiales@gmail.com>
2025-03-03 21:02:17 -0300

    Code update

--- recommender_system/assets/airbyte/remote_movies_users.py
+++ recommender_system/assets/airbyte/remote_movies_users.py
@@ -7,22 +7,92 @@ import pandas as pd
 from dagster_airbyte import build_airbyte_assets
 from dagster import AssetExecutionContext
 
+#### 
+# dbt
+###
+# from pathlib import Path
 
-# Replace with the actual connection IDs from Airbyte
+# from dagster import AssetExecutionContext
+# from dagster_dbt import DbtCliResource, dbt_assets
+
+# DBT_PROJECT_DIR = file_relative_path(__file__, "../../dbt_project")
+# DBT_PROJECT_DIR = "/Users/hordia/dev/MLOps-itba/dbt_mlops/"
+# dbt_resource = DbtCliResource(project_dir=DBT_PROJECT_DIR)
+# dbt_parse_invocation = dbt_resource.cli(["parse"]).wait()
+# DBT_MANIFEST_PATH = dbt_parse_invocation.target_path.joinpath("manifest.json")
+# @dbt_assets(
+#     manifest=DBT_MANIFEST_PATH,
+#     # io_manager_key="db_io_manager",
+# )
+# def dbt_project_assets(context: AssetExecutionContext, dbt: DbtCliResource):
+#     yield from dbt.cli(["dev"], context=context).stream()
+#     #yield from dbt.cli(["build"], context=context).stream()
+# ########
+
+# # Replace with the actual connection IDs from Airbyte
 airbyte_movies = build_airbyte_assets(
     connection_id="99411b7e-ddec-495c-ae57-9849ec67875e",
-    destination_tables=["airbyte_movies"]
+    destination_tables=["airbyte_movies"],
+    # auto_materialize_policy="eager"  
 )
+
+# test ids
+# movies / connection_id="99411b7e-ddec-495c-ae57-9849ec67875e",
+
+#alt ids
+#movies = 1d4fd1c5-33eb-4b1f-bbaa-41b3870ff51e
+
+# Define Airbyte asset
+# @asset
+# def airbyte_movies(context):
+#     assets = build_airbyte_assets(
+#         connection_id="99411b7e-ddec-495c-ae57-9849ec67875e",
+#         destination_tables=["airbyte_movies"]
+#         #,
+#         #auto_materialize_policy="eager"  # 
+#     )
+
+#     #Return an Output object for tracking
+#     return Output(
+#         #value=assets,  # This holds the Airbyte assets
+#         value=None,  # This holds the Airbyte assets
+#         metadata={"description": "Movies data synced from Airbyte"}
+#     )
+
 
 airbyte_users = build_airbyte_assets(
     connection_id="e66616fb-8642-4e3a-9102-f6a67b088e32",
     destination_tables=["airbyte_users"]
 )
 
+# Ejecutado asi, ejecuta en airbyte, recibe data del proceso pero falta el Output y falla el STEP
 airbyte_scores = build_airbyte_assets(
     connection_id="b149a0f1-d8ea-44da-9320-0a971a1e7f99",
     destination_tables=["airbyte_scores"]
 )
+
+# from dagster_airbyte import airbyte_sync_op
+
+# @asset
+# def airbyte_scores(context):
+#     """Trigger Airbyte sync and return metadata but no binary output."""
+
+#     # Trigger the Airbyte sync
+#     airbyte_result = airbyte_sync_op(context, connection_id="b149a0f1-d8ea-44da-9320-0a971a1e7f99")
+
+#     # Extract metadata from the Airbyte sync result
+#     sync_status = airbyte_result.get("status", "Unknown")
+#     sync_id = airbyte_result.get("job_info", {}).get("job", {}).get("id")
+
+#     context.log.info(f"Airbyte sync triggered: Status = {sync_status}, Job ID = {sync_id}")
+
+#     return Output(
+#         value=None,  # No binary output
+#         metadata={
+#             "airbyte_sync_status": sync_status,
+#             "airbyte_job_id": sync_id
+#         }
+#     )
 
 # Group all Airbyte assets
 # Agrupa los assets de Airbyte para usarlos en Dagster
--- recommender_system/definitions.py
+++ recommender_system/definitions.py
@@ -2,11 +2,12 @@ from dagster import Definitions, load_assets_from_modules, define_asset_job, Ass
 from recommender_system.configs import job_data_config, job_training_config
 from recommender_system.assets import (
     core_assets, recommender_assets, airbyte_assets
-#, dbt_transformations
 )
+# from recommender_system.assets.airbyte.dbt import dbt_models, dbt_resource
 
 from dagster import EnvVar
 from dagster_airbyte import AirbyteResource, load_assets_from_airbyte_instance
+
 # Definir el recurso de Airbyte
 airbyte_resource = AirbyteResource(
     host="localhost",
@@ -15,12 +16,10 @@ airbyte_resource = AirbyteResource(
     password=EnvVar("AIRBYTE_PASSWORD")
 )
 
-# airbyte_assets = load_assets_from_airbyte_instance(airbyte_resource)
-
 # all_assets = load_assets_from_modules([movies_users])
 # all_assets = [*core_assets, *recommender_assets]
 all_assets = [*core_assets, *recommender_assets, *airbyte_assets]
-
+# all_assets = [*recommender_assets, *airbyte_assets, dbt_models]
 
 mlflow_resources = {
     'mlflow': {
@@ -29,7 +28,6 @@ mlflow_resources = {
         }            
     },
 }
-
 
 data_job = define_asset_job(
     name='get_data',
@@ -55,9 +53,15 @@ from dagster import fs_io_manager
 
 airbye_job = define_asset_job(
     name="airbye_job",
-    selection=AssetSelection.groups('airbyte'),
+    selection=AssetSelection.assets(*airbyte_assets),
+    # selection=AssetSelection.groups('airbyte'),
     # config=job_airbyte_config
 )
+
+# dbt_job = define_asset_job(
+#     name="dbt_job",
+#     selection=AssetSelection.assets(dbt_models),
+# )
 
 recommender_job = define_asset_job(
     name="only_training",
@@ -66,33 +70,19 @@ recommender_job = define_asset_job(
     config=job_training_config
 )
 
-# defs = Definitions(
-#     assets=[*airbyte_assets],  # ✅ Ensure assets are properly loaded
-#     resources={  
-#         "airbyte": airbyte_resource,  # ✅ Airbyte Resource
-#         "io_manager": fs_io_manager,  # ✅ Fix missing io_manager
-#         **job_data_config["resources"],  # ✅ Other resources (e.g., MLflow)
-#     },
-# )
 defs = Definitions(
     assets=all_assets,
     jobs=[
         data_job,
-        recommender_job
-        ,
-        airbye_job
+        recommender_job,
+        airbye_job,
+        # dbt_job
     ],
     resources={  
         "airbyte": airbyte_resource, 
-        "io_manager": fs_io_manager, 
+        "io_manager": fs_io_manager,
+        # "dbt": dbt_resource,
+        # "db_io_manager": DbIOManager(**POSTGRES_CONFIG),
         # **job_data_config["resources"], #no agregar esto para no duplicar key
     }
 )
-
-# defs = Definitions(
-#     assets=[*airbyte_assets],  # Agregar los assets de Airbyte
-#     resources={
-#         "airbyte": airbyte_resource,  # Incluir el recurso de Airbyte
-#         **job_data_config["resources"]
-#     },
-# )
